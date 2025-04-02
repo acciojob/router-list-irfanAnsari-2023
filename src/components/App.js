f9547c233edbb03fdc3122d68fe8a81b7694fd75
Irfan Ansari <123730480+irfanAnsari-2023@users.noreply.github.com>
2025-04-02 20:28:04 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ItemList from "./ItemList";
 import ItemDetail from "./ItemDetail";
-import "./styles/App.css";
+import './../styles/App.css';
 
 const App = () => {
   return (
